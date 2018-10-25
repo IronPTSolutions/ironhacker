bfb0665aded0ad0d10cd3a8a0935518503f79861
Carlos del Prado <carlos.delprado@11paths.com>
2018-10-25 19:23:54 +0200

    feat: ironhacker module

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const cowsay = require('cowsay');
+const readline = require('readline-sync');
+const homedir = require('os').homedir();
+const fs = require('fs');
+const IronHackers = require('./models/ironhackers');
+
+try {
+  const students = JSON.parse(fs.readFileSync(`${homedir}/.ironhackers.json`, 'utf8'));
+  const ironhackers = new IronHackers(students);
+
+  console.clear();
+  let input;
+  while (input !== 'exit') {
+    console.log(cowsay.think({ text: ironhackers.next(), e: "oO" }));
+    input = readline.question('');
+    console.clear();
+  }
+} catch (error) {
+  console.error('Invalid ironhackers.json file:', error.message);
+}
